69892cf9ca0b5d4935397ff66db85cf8560781a2
cumbinar <cumbiweb@gmail.com>
2022-04-15 09:04:52 -0500

    Curso JavaScript 05 Array Methods

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log('Hola Cumbi');
+
+//some//
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto'];
+
+const carrito = [
+    { nombre: 'Guitarra', precio: 500 },
+    {nombre: 'Guitarra', precio: 500},
+]
+
+
